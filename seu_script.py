c42ee0e1ca7891dd3bb113eb9324eeceb7af3f9e
renangadelha12 <118527388+renangadelha12@users.noreply.github.com>
2024-12-09 09:02:17 -0300

    Add files via upload

--- seu_script.py
+++ seu_script.py
@@ -3,68 +3,56 @@ from dash import dcc, html
 import plotly.express as px
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.figure_factory as ff
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
-import pandas as pd
-import plotly.express as px
-import dash_bootstrap_components as dbc
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-import pandas as pd
-import numpy as np
-import plotly.express as px
-topo = ['Data',
-'Hora',
-'Temperatura',
-'Temp. Máxima',
-'Temp. Mínima',
-'Umidade',
-'Ponto de Orvalho',
-'Velocidade do Vento',
-'Direção do Vento',
-'Distância Percorrida pelo Vento',
-'Velocidade Máxima',
-'Direção Máxima',
-'Sensação Térmica',
-'Índice de Calor',
-'Índice THW',
-'Índice THSW',
-'Pressão Atmosférica',
-'Precipitação',
-'Taxa de Chuva',
-'Radiação Solar',
-'Energia Solar',
-'Radiação Solar Máxima',
-'UVI',
-'Dose UV',
-'Índice UV Máximo',
-'Grau-dias de Aquecimento',
-'Grau-dias de Resfriamento',
-'Temp. Interna',
-'Umidade Interna',
-'Ponto de Orvalho Interno',
-'Calor Interno',
-'EMC Interno (Conteúdo de Umidade em Equilíbrio)',
-'Densidade do Ar Interna',
-'Evapotranspiração',
-'Amostragem do Vento',
-'Transmissão do Vento',
-'Receptor ISS',
-'Intervalo de Arquivamento']
 
+topo = ['Date',
+'Time',
+'Temperatura',
+'Hi temp',
+'Low Temp',
+'Umidade',
+'Dew Pt.',
+'Velocidade do Vento',
+'Wind Dir',
+'Wind Run',
+'Hi Speed',
+'Hi Dir',
+'Wind Chill',
+'Heat Index',
+'THW Index',
+'THSW Index',
+'Pressão Atm.',
+'Precipitação',
+'Rain Rate',
+'Solar Rad',
+'Solar Energy',
+'Hi Solar Rad',
+'UVI',
+'UV Dose',
+'Hi UV',
+'Heat D-D',
+'Cool D-D',
+'In Temp',
+'In Hum',
+'In Dew',
+'In Heat',
+'In EMC',
+'In Air Density',
+'ET',
+'Wind Samp',
+'Wind TX',
+'ISS Recept',
+'Arc Int.']
 if 1!=2:
-    dados='davis_teste_dash.txt'
+    dados='davis_teste.txt'
     
     davis = pd.read_csv(dados, sep='\t', dtype={'Temp OUT': float, 'Out Hum': float, 'Rain': float, 'UV Index': float,'UV Dose':float,'Bar':float,'Dew Pt.':float,'Heat Index':float}, na_values=['---','------'], header=1,names=topo)
 
     #davis=davis.sort_values(by='Date')
-    davis["Data"] = pd.to_datetime(davis["Data"])
-    davis['Dia']=davis['Data'].dt.day
-    davis['Year']=davis['Data'].dt.year
-    davis['Mês']=davis['Data'].dt.month
-    davis['Mês2']=davis['Data'].dt.month
+    davis["Date"] = pd.to_datetime(davis["Date"])
+    davis['Dia']=davis['Date'].dt.day
+    davis['Year']=davis['Date'].dt.year
+    davis['Mês']=davis['Date'].dt.month
+    davis['Mês2']=davis['Date'].dt.month
     davis['Year']= pd.to_numeric(davis['Year'], errors='coerce')
    # bar_avg=davis[['Bar','Year']].groupby('Year').mean()
     
@@ -76,11 +64,6 @@ if 1!=2:
     
     davis = davis[davis['Year'] != 2018]'''
 
-davis['Data'] = pd.to_datetime(davis['Data'])
-
-# Extrai a data no formato 'YYYY-MM-DD'
-dias = davis['Data'].dt.strftime('%Y-%m-%d')
-dias_lista=dias.unique()
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
@@ -96,24 +79,15 @@ messes = davis['Mês'].unique()
 messes_lista = list(messes)
 mes_selecionado = messes_lista[0]
 ##############
-d_t_mes=davis[['Data','Year','Dia','Mês2','UVI']]
-d_t_mes=d_t_mes.sort_values(by='Data')
-d_t_mes=d_t_mes.sort_values(by='Data')
+d_t_mes=davis[['Date','Year','Dia','Mês2','UVI']]
+d_t_mes=d_t_mes.sort_values(by='Date')
+d_t_mes=d_t_mes.sort_values(by='Date')
 anos_do_df=d_t_mes['Year'].unique()
 messes_do_df=d_t_mes['Mês2'].unique()
 anos_do_df_lista=list(anos_do_df)
 dias_do_df=d_t_mes['Dia'].unique()
 dias_do_df_lista=list(dias_do_df)
 topo_lista=list(topo)
-
-tipo_de_analise = ['Média', 'Máximo', 'Mínimo']
-tipo_lista = list(tipo_de_analise)
-data = {
-    'Ano': davis['Year'],
-    'Mês': davis['Mês2'],
-    'Variável': davis['UVI']
-}
-df = pd.DataFrame(data)
 
 app.layout = dbc.Container([
     html.H2(children='Selecione um ano'),
@@ -130,7 +104,7 @@ app.layout = dbc.Container([
 
     dbc.Row([
         dbc.RadioItems(
-            options=[{"label": x, "value": x} for x in ['Pressão Atmosférica', 'Temperatura', 'Umidade', 'Velocidade do Vento', 'Precipitação', 'UVI']],
+            options=[{"label": x, "value": x} for x in ['Pressão Atm.', 'Temperatura', 'Umidade', 'Velocidade do Vento', 'Precipitação', 'UVI']],
             value='Temperatura',
             inline=True,
             id='radio-buttons-final'
@@ -151,14 +125,28 @@ app.layout = dbc.Container([
     dcc.Graph(id='pie-chart')
     ]),
     html.H1(children='Para fazer uma análise em um dia específico, selecione um dia nos quadros abaixo.'),
+    html.H4(children='Aqui selecione um Ano:'),
     
+    dcc.Dropdown(
+        id='a-dropdown',
+        options=[{'label': str(anos_do_df), 'value': anos_do_df} for anos_do_df in anos_do_df_lista],
+        value=anos_do_df[0]
+    ),
+    html.H4(children='Aqui selecione um Mês:'),
+
+    dcc.Dropdown(
+        id='messes-dropdown',
+        options=[{'label': str(messes), 'value': messes} for messes in messes_lista],
+        value=messes[0]
+    ),
     html.H4(children='Aqui selecione um Dia:'),
 
     dcc.Dropdown(
-        id='dia_nv-dropdown',
-        options=[{'label': str(dias), 'value': dias} for dias in dias_lista],
-        value=dias[0]
+        id='dia-dropdown',
+        options=[{'label': str(dias_do_df), 'value': dias_do_df} for dias_do_df in dias_do_df_lista],
+        value=messes[0]
     ),
+    
     html.Div(id='output-container2'),
     html.Div([
     html.H2("Gráfico Selecionável"),
@@ -170,20 +158,6 @@ app.layout = dbc.Container([
     ),
     dcc.Graph(id='fig3')
     ]),
-    html.H1("Heatmap de Análises Mensais por Ano"),
-    html.H4(children='Entre com a Variável que você deseja verificar:'),
-    dcc.Dropdown(
-        id='variaveis-2',
-        options=[{'label': str(topo), 'value': topo} for topo in topo_lista],
-        value=topo_lista[3],  # Ajuste conforme necessário
-    ),
-    html.H4(children='Entre com o tipo de análise (Máximo, Média ou Mínimo):'),
-    dcc.Dropdown(
-        id='tipo_de_analise-2',
-        options=[{'label': tipo, 'value': tipo} for tipo in tipo_lista],
-        value=tipo_lista[0],  # Valor padrão
-    ),
-    dcc.Graph(id='heatmap-graph')
     
 ], fluid=True)
 
@@ -191,29 +165,28 @@ app.layout = dbc.Container([
     [Output(component_id='my-first-graph-final', component_property='figure'),
      Output('pie-chart', 'figure'),
      Output('fig3', 'figure'),
-     Output('heatmap-graph', 'figure'),
      Output('output-text', 'children'),
      Output('my-table', 'columns'),
      Output('my-table', 'data'),
-     #Output(component_id='heatmap-graph', component_property='figure'),
      
      ],
     [Input(component_id='anos-dropdown', component_property='value'),
      Input(component_id='radio-buttons-final', component_property='value'),
-     Input(component_id='dia_nv-dropdown', component_property='value'),
+     Input(component_id='messes-dropdown', component_property='value'),
+     Input(component_id='a-dropdown', component_property='value'),
+     Input(component_id='dia-dropdown', component_property='value'),
      Input(component_id='variaveis', component_property='value'),
-     Input(component_id='variaveis-2', component_property='value'),
-     Input(component_id='tipo_de_analise-2', component_property='value')
      ]
 )
-def update_output(selected_year, col_chosen,dia_s,variavel,v_selected,s_type):    
+def update_output(selected_year, col_chosen, selected_mes,selected_ano,selected_dia,variavel):
     global davis2
-    global s_day
+    global ano_selecionado
+    global mes_selecionado
+    global dia_selecionado
     ano_selecionado = selected_year
-    variavel_heatmap=v_selected
-    static=s_type
-
-    s_day=dia_s
+    ano_especifico=selected_ano
+    mes_selecionado = selected_mes
+    dia_selecionado=selected_dia
     variavels=variavel
     
     
@@ -221,12 +194,12 @@ def update_output(selected_year, col_chosen,dia_s,variavel,v_selected,s_type):
     
     d_dash_max = davis2[['Mês2', 'Mês', 'UVI']].groupby('Mês2').max()
     d_dash_sum = davis2[['Mês2', 'Precipitação']].groupby('Mês2').sum()
-    d_dash_médias = davis2[['Mês2', 'Mês', 'Pressão Atmosférica', 'Temperatura', 'Umidade', 'Velocidade do Vento', 'Precipitação', 'UVI']].groupby('Mês2').mean()
+    d_dash_médias = davis2[['Mês2', 'Mês', 'Pressão Atm.', 'Temperatura', 'Umidade', 'Velocidade do Vento', 'Precipitação', 'UVI']].groupby('Mês2').mean()
     d_dash_precp = pd.DataFrame({
         'Mês': d_dash_médias['Mês'],
         'Precipitação': d_dash_sum['Precipitação']
     })
-    d_table_p1 = davis2[['Mês2', 'Mês', 'Pressão Atmosférica', 'Temperatura', 'Umidade', 'Velocidade do Vento']].groupby('Mês2').mean()
+    d_table_p1 = davis2[['Mês2', 'Mês', 'Pressão Atm.', 'Temperatura', 'Umidade', 'Velocidade do Vento']].groupby('Mês2').mean()
     d_table_p2 = davis2[['Mês2', 'Precipitação']].groupby('Mês2').sum()
     d_table_p3 = davis2[['Mês2', 'UVI']].groupby('Mês2').max()
     
@@ -235,7 +208,7 @@ def update_output(selected_year, col_chosen,dia_s,variavel,v_selected,s_type):
     d_t = pd.DataFrame({
         'Mês': d_table_p1['Mês'],
         'Mês_c': d_table_p1['Mês'],
-        'Pressão Atmosférica (hPa).': d_table_p1['Pressão Atmosférica'],
+        'Pressão Atm (hPa).': d_table_p1['Pressão Atm.'],
         'Temperatura (°C)': d_table_p1['Temperatura'],
         'Umidade (%)': d_table_p1['Umidade'],
         'Velocidade do Vento (m/s)': d_table_p1['Velocidade do Vento'],
@@ -243,10 +216,15 @@ def update_output(selected_year, col_chosen,dia_s,variavel,v_selected,s_type):
         'UVI': d_table_p3['UVI'],
     })
     
-    output_text = f'Dash Board Davis LAVAT - Ano Observado: {selected_year} - {variavel_heatmap} -{static} '
-    
-    dia_escholido_dropdown=davis[davis['Data'] == s_day]
-    dia_escholido_dropdown["Date"] = pd.to_datetime(dia_escholido_dropdown["Data"])
+    output_text = f'Dash Board Davis LAVAT - Ano Observado: {selected_year} '
+    if mes_selecionado <10:
+        mes_selecionado=f'0{mes_selecionado}'
+    if dia_selecionado<10:
+        dia_selecionado=f'0{dia_selecionado}'
+
+    dia_select=f'{ano_especifico}-{mes_selecionado}-{dia_selecionado}'
+    dia_escholido_dropdown=davis[davis['Date'] == dia_select]
+    dia_escholido_dropdown["Date"] = pd.to_datetime(dia_escholido_dropdown["Date"])
     #dia_escholido_dropdown=dia_escholido_dropdown.sort_values(by='Time')
     #dia_escholido_dropdown=davis[davis['Date'] == '2023-12-12']
     d_t['Mês_c'] = d_t['Mês_c'].replace({1: 'Janeiro', 2: 'Fevereiro', 3:'Março', 4: 'Abril', 5: 'Maio', 6: 'Junho',
@@ -263,49 +241,17 @@ def update_output(selected_year, col_chosen,dia_s,variavel,v_selected,s_type):
     
    
     fig2 = px.pie(d_t, values='Precipitação (mm/mês)', names=messes_c, title='Distribuição Pluviométrica')
-    fig3 = px.line(dia_escholido_dropdown, x=dia_escholido_dropdown['Hora'], y=variavels)
-
-    df['Variável'] = davis[variavel_heatmap]
-    selected_analysis=static
-    # Definindo a função de agregação com base no tipo de análise selecionada
-    if selected_analysis == 'Média':
-        agg_func = 'mean'
-    elif selected_analysis == 'Máximo':
-        agg_func = 'max'
-    elif selected_analysis == 'Mínimo':
-        agg_func = 'min'
-
-    # Calculando a análise da variável por Ano e Mês
-    df_pivot = df.pivot_table(index='Ano', columns='Mês', values='Variável', aggfunc=agg_func)
-    
-    # Garantir que todos os meses de 1 a 12 estejam presentes
-    df_pivot = df_pivot.reindex(columns=np.arange(1, 13))
-
-    # Gerando o heatmap
-    fig_h = px.imshow(df_pivot, 
-                    labels=dict(x="Mês", y="Ano", color=selected_analysis),
-                    x=df_pivot.columns,  # Colunas são os meses
-                    y=df_pivot.index,    # Linhas são os anos
-                    aspect="auto",
-                    text_auto=True)
-
-    # Atualizando layout para garantir que todos os anos apareçam no eixo y
-    fig_h.update_yaxes(tickvals=df_pivot.index,  # Garantindo que todos os anos sejam mostrados
-                     ticktext=[str(year) for year in df_pivot.index])  # Convertendo anos para string
-    
-    fig_h.update_layout(title=f'{selected_analysis} Mensais de {variavel_heatmap} ao Longo dos Anos',
-                      xaxis_title="Mês",
-                      yaxis_title="Ano")
-
-
+    fig3 = px.line(dia_escholido_dropdown, x=dia_escholido_dropdown['Time'], y=variavels)
 
     columns = [{'name': col, 'id': col} for col in d_t.columns]
     data = d_t.reset_index().to_dict('records')
     
-    return fig, fig2,fig3,fig_h, output_text, columns, data
+    print(dia_escholido_dropdown)
+    return fig, fig2,fig3, output_text, columns, data
+
 
 
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=8052)
